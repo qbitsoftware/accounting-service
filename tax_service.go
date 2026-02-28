90d55e4f3cebe949e81070703001e534088c8240
pr22nik23 <harrimahar@gmail.com>
2026-02-28 21:11:20 +0200

    expose listaccounts through taxservice

--- tax_service.go
+++ tax_service.go
@@ -9,3 +9,8 @@ type TaxService struct {
 func (s *TaxService) List(ctx context.Context) ([]Tax, error) {
 	return s.provider.ListTaxes(ctx)
 }
+
+// ListAccounts returns the chart of accounts (GL accounts) from the provider.
+func (s *TaxService) ListAccounts(ctx context.Context) ([]Account, error) {
+	return s.provider.ListAccounts(ctx)
+}
